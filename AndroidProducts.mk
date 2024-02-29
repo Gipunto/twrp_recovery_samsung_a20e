1ef376d2f30231107e169182b9c99d5b3e73d9ce
Gipunto <157641367+Gipunto@users.noreply.github.com>
2024-02-29 21:34:00 +0100

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a20e.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a20e-user \
+    omni_a20e-userdebug \
+    omni_a20e-eng
